2ef7997077f7286de44c01a5ef62d3bf6fca40b0
SUS4N00 <cuong0345259984@gmail.com>
2024-03-16 17:24:48 +0700

    update enemy move

--- Assets/_Scripts/enemy/enemyMove.cs
+++ Assets/_Scripts/enemy/enemyMove.cs
@@ -1,14 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Http.Headers;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class enemyMove : MonoBehaviour
 {
     public LayerMask playerLayer;
     private float horizontalValue = 0f;
     public float changeSpeed = 0.1f;
-    public Rigidbody2D enemySelf;
     private Animator anim;
     public Transform playerPosition;
     public Transform attackPoint;
@@ -24,31 +25,29 @@ public class enemyMove : MonoBehaviour
     public bool isFacingRight = false;
     public AudioManager audioManager;
     public float move;
+    private bool foundedTarget;
+    public float timer;
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0;
+        foundedTarget = false;
         playerPosition = GameObject.Find("player").transform;
         audioManager = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioManager>();
-        enemySelf = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-
-    public bool seePlayer(){
-        if(isFacingRight && playerPosition.transform.position.x >= transform.position.x || !isFacingRight && playerPosition.transform.position.x < transform.position.x){
-            return true;
-        }else{
-            return false;
-        }
+    void Update(){
+        timer += Time.deltaTime;
+        findTarget();
     }
     void FixedUpdate(){
         if(Time.time >= nextTimeAttack){
             Move();
         }
         if(attackPoint.position.x < playerPosition.position.x+attackRange+0.3f && attackPoint.position.x > playerPosition.position.x-attackRange-0.3f){
-            if(Time.time >= nextTimeAttack && move == 0 && !GetComponent<enemy>().hited){
-                findAndAttack();
+            if(Time.time >= nextTimeAttack && move == 0 && !GetComponent<enemy>().hited && foundedTarget){
+                Attack();
             }
         }
         if(GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsTag("attack") || GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsTag("attack&hurt")){
@@ -58,7 +57,64 @@ public class enemyMove : MonoBehaviour
         }
     }
 
-    void findAndAttack(){
+    public bool seePlayer(){
+        if(isFacingRight && playerPosition.transform.position.x >= transform.position.x || !isFacingRight && playerPosition.transform.position.x < transform.position.x){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private void findTarget(){
+        if(foundedTarget == false){
+            if(timer > 2 && timer <4){
+                move = -2;
+            }else if(timer > 6 && timer < 8){
+                move = 2;
+            }else if(timer > 8){
+                timer -= timer;
+            }else{
+                move = 0;
+            }
+            if(gameObject.tag == "canChien" || gameObject.tag == "canChien2"){
+                if(seePlayer() == true && Mathf.Abs(attackPoint.position.x - playerPosition.position.x) <= attackRange * 3.5f){
+                    foundedTarget = true;
+                }
+            }else if(gameObject.tag == "tamXa"){
+                if(seePlayer() == true && Mathf.Abs(attackPoint.position.x - playerPosition.position.x) <= attackRange){
+                    foundedTarget = true;
+                }
+            }
+            if(GetComponent<enemy>().hited || anim.GetCurrentAnimatorStateInfo(0).IsTag("attack")){
+                foundedTarget = true;
+            }
+        }else{
+            if(gameObject.tag == "canChien" || gameObject.tag == "canChien2"){
+                if(attackPoint.position.x > playerPosition.position.x + attackRange + 0.16f){
+                    move = -5;
+                }else if(attackPoint.position.x < playerPosition.position.x - attackRange - 0.16f){
+                    move = 5;
+                }else{
+                    move = 0f;
+                }
+            }else if(gameObject.tag == "tamXa"){
+                if(attackPoint.position.x > playerPosition.position.x + attackRange + 0.16f){
+                    move = -5;
+                }else if(attackPoint.position.x < playerPosition.position.x - attackRange - 0.16f){
+                    move = 5;
+                }else{
+                    if(isFacingRight && attackPoint.position.x > playerPosition.position.x + 0.2f){
+                        move = -1;
+                    }else if(!isFacingRight && attackPoint.position.x < playerPosition.position.x - 0.2f){
+                        move = 1;
+                    }else{
+                        move = 0;
+                    }
+                }
+            }
+        }
+    }
+    void Attack(){
         //atacking = true;
         nextTimeAttack = Time.time + 1/attackRate;
         //animation
@@ -72,29 +128,6 @@ public class enemyMove : MonoBehaviour
         }
         if(GetComponent<enemy>().hited){
             move = 0;
-        }
-        if(gameObject.tag == "canChien" || gameObject.tag == "canChien2"){
-            if(attackPoint.position.x > playerPosition.position.x + attackRange + 0.16f){
-                move = -5;
-            }else if(attackPoint.position.x < playerPosition.position.x - attackRange - 0.16f){
-                move = 5;
-            }else{
-                move = 0f;
-            }
-        }else if(gameObject.tag == "tamXa"){
-            if(attackPoint.position.x > playerPosition.position.x + attackRange + 0.16f){
-                move = -5;
-            }else if(attackPoint.position.x < playerPosition.position.x - attackRange - 0.16f){
-                move = 5;
-            }else{
-                if(isFacingRight && attackPoint.position.x > playerPosition.position.x + 0.2f){
-                    move = -1;
-                }else if(!isFacingRight && attackPoint.position.x < playerPosition.position.x - 0.2f){
-                    move = 1;
-                }else{
-                    move = 0;
-                }
-            }
         }
         if(move != 0f && atacking == false && !GetComponent<enemy>().hited){
             anim.SetBool("move", true);
